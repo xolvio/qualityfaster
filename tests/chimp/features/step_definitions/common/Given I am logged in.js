7663747d353b8d9977b646dc62c6c167e9454d03
Sam Hatoum <sam@xolv.io>
2015-12-25 02:20:59 -0800

    Removes accidentally added files

--- tests/chimp/features/step_definitions/common/Given I am logged in.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-  this.Given(/^I am logged in$/, function () {
-    fixtures.accountHolders.createAndLogin({
-      username: 'mememe',
-      password: 'acc0untH0lder'
-    });
-  });
-};
--- tests/chimp/features/support/hooks.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  this.Before(function() {
-
-    fixtures.common.reset();
-
-  });
-};
