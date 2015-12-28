fae395ee75c252e106303e01c049a0c2c05a4717
Sam Hatoum <sam@xolv.io>
2015-12-28 08:14:28 -0500

    Adds Firefox to Karma lanuchers

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox', 'Chrome'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
