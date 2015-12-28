64c3578a70f5b1ebbbc76713fb6bdbe53afbf95c
Sam Hatoum <sam@xolv.io>
2015-12-28 10:18:07 -0500

    Fixes Travis chrome

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.jsx': [ 'react-jsx' ]
+      '**/*.jsx': ['react-jsx']
     },
 
     // test results reporter to use
@@ -39,14 +39,21 @@ module.exports = function (config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_CI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -55,5 +62,11 @@ module.exports = function (config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
+  });
+
+  if (process.env.CI || process.env.TRAVIS || process.env.CIRCLECI) {
+    config.browsers = ['Chrome_CI', 'Firefox'];
+    config.singleRun = true;
+  }
+
 };
