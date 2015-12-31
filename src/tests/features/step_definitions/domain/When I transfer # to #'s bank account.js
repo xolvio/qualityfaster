1c093f29642a890cb517307922f5bd6a21b277ee
Sam Hatoum <sam@xolv.io>
2015-12-30 21:45:33 -0500

    Removes extra spaces

--- src/tests/features/step_definitions/domain/When I transfer # to #'s bank account.js
+++ src/tests/features/step_definitions/domain/When I transfer # to #'s bank account.js
@@ -2,10 +2,8 @@ module.exports = function () {
   this.When(/^I transfer \$(\d+) to "([^"]*)"'s bank account$/, function (amount, otherAccountHolderName) {
     var toAccountHolderId = this.accounts[otherAccountHolderName]._id;
     this.bankTransferResult = server.execute(function (amount, toAccountHolderId) {
-
       const fromAccountHolderId = Meteor.userId();
       return BankService.transfer(fromAccountHolderId, toAccountHolderId, amount);
-
     }, amount, toAccountHolderId);
   });
 };
