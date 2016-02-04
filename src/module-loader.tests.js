2bb9bd95d2a194e086c5f58adff374d46062680a
Sam Hatoum <sam@xolv.io>
2016-02-03 21:56:15 -0800

    Partial fix for isomorphic service issues

--- src/module-loader.tests.js
+++ src/module-loader.tests.js
@@ -2,8 +2,13 @@
 
 // Force meteor to load modules at build time, so they can be required in Chimp's server.execute
 if (false) {
+  require('./imports/validation');
   require('./imports/models/collections');
   require('./imports/models/account-holder');
   require('./imports/services/bank-service');
+  require('./imports/services/bank-service/server');
+  //require('./imports/services/bank-service/client');
   require('./imports/services/account-service');
+  require('./imports/services/account-service/server');
+  //require('./imports/services/account-service/client');
 }
