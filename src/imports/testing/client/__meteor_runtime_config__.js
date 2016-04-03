5181e9aee98da58ad54370431ce8f93e6e4e35c5
Andżelo Mazurek <andre2go@gmail.com>
2016-04-03 11:06:23 +0200

    fix typo in NODE_ENV

--- src/imports/testing/client/__meteor_runtime_config__.js
+++ src/imports/testing/client/__meteor_runtime_config__.js
@@ -3,7 +3,7 @@
 
 window.__meteor_runtime_config__ = {
   meteorEnv: {
-    "NODE_ENV": process.env.NODE_ENV || "develompent"
+    "NODE_ENV": process.env.NODE_ENV || "development"
   },
   "meteorRelease": "METEOR@1.3",
   "PUBLIC_SETTINGS": {},
