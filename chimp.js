0f9d9f902d82c014a09d0802737afc069d1e369e
Andżelo Mazurek <andre2go@gmail.com>
2016-04-03 11:01:23 +0200

    fix __meteor_runtime_config__

--- chimp.js
+++ chimp.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   watchTags: '@focus',
-  path: path.resolve('tests/features'),
-  domainSteps: path.resolve('tests/features/step_definitions/domain'),
-  criticalSteps: path.resolve('tests/features/step_definitions/critical')
+  path: path.resolve('tests'),
+  domainSteps: path.resolve('tests/step_definitions/domain'),
+  criticalSteps: path.resolve('tests/step_definitions/critical')
 };
