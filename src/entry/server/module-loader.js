2e2742ad39428651740d2e2253be107fb350cb30
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-04-09 19:10:28 -0700

    Removes forced module loading that is no longer needed

--- src/entry/server/module-loader.js
+++ src/entry/server/module-loader.js
@@ -1,10 +1,4 @@
 // Force meteor to load modules at build time, so they can be required in Chimp's server.execute
 if (false) {
-  require('./imports/validation');
-  require('./imports/models/account-holder');
-  require('./imports/domain/services/bank-service');
-  require('./imports/domain/services/bank-service/server');
-  require('./imports/application/services/account-service');
-  require('./imports/application/services/account-service/server');
-  require('./imports/application/services/bank-service/server');
+  
 }
