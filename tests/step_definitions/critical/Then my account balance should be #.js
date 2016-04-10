ef210218e67c59c613a78e60dd6b5bbc7472d43f
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-04-09 19:23:43 -0700

    Reverts wrong spec automation

--- tests/step_definitions/critical/Then my account balance should be #.js
+++ tests/step_definitions/critical/Then my account balance should be #.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   this.Then(/^my account balance should be \$(\d+)$/, function (balance) {
     var actualBalance = widgets.accountSummary.getBalance();
-    expect(actualBalance).toEqual(balance + 1);
+    expect(actualBalance).toEqual(balance);
   });
 };
