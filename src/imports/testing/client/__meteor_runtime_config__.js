37b1d2f9fffa472e6e8aa2563c2e01c0766ac2b2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-05-13 19:33:47 +0200

    Fixes __meteor_runtime_config__

--- src/imports/testing/client/__meteor_runtime_config__.js
+++ src/imports/testing/client/__meteor_runtime_config__.js
@@ -1,17 +1,21 @@
 /* globals __meteor_runtime_config__: true */
 /* jshint camelcase: false */
 
+// You get the runtime config by opening your app in the browser
+// and then executing `JSON.stringify(window.__meteor_runtime_config__, null, '  ')`
+// in the developer tools console.
 window.__meteor_runtime_config__ = {
-  meteorEnv: {
-    "NODE_ENV": process.env.NODE_ENV || "development"
-  },
   "meteorRelease": "METEOR@1.3",
+  "meteorEnv": {
+    "NODE_ENV": "development",
+    "TEST_METADATA": "{}"
+  },
   "PUBLIC_SETTINGS": {},
   "ROOT_URL": "http://localhost:3000/",
   "ROOT_URL_PATH_PREFIX": "",
   "appId": "16bfdn3i3fnqt1gmmf05",
-  "autoupdateVersion": "eea49fd4c1894a38d5aee49eb8491e361e9c8afd",
-  "autoupdateVersionRefreshable": "bbc047ad9f781ff29b00fef4796289732382cad4",
+  "autoupdateVersion": "dffb3dfbfa7f3c6e2ee78a1bcd0a2c7ea7f7f012",
+  "autoupdateVersionRefreshable": "a7c6994f68c1ac79bd7a349dc01a58ad30617ec4",
   "autoupdateVersionCordova": "none"
 };
 
