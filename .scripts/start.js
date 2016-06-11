5d4184cf11db1c8b6f5e5f622e3fbf29c1b6efd2
zVictor <victor0110@msn.com>
2016-06-11 09:57:10 +0200

    add Jasmine and Mocha to npm start

--- .scripts/start.js
+++ .scripts/start.js
@@ -10,7 +10,8 @@ var baseDir = path.resolve(__dirname, '..'),
    chimpBin = path.resolve(baseDir, '.scripts/node_modules/.bin/chimp'),
    features = [];
 
-process.argv.slice(2).forEach(function(featureFile) { features.push(path.resolve(featureFile))});
+var argv = require('minimist')(process.argv.slice(2));
+argv._.forEach(function(featureFile) { features.push(path.resolve(featureFile))});
 
 var appOptions = {
   settings: 'settings.json',
@@ -31,69 +32,105 @@ var mirrorOptions = {
   logFile: './chimp-mirror.log'
 };
 
-
-var chimpSwitches = '';
-
-if (features.length > 0) {
-  chimpSwitches += ' --path=' + path.resolve('tests') + ' ' + features.join(" ");
+if (argv.mocha) {
+  startMocha();
+} else if (argv.jasmine) {
+  startJasmine();
 } else {
-  chimpSwitches = ' --path=' + path.resolve('tests/specifications') + chimpSwitches;
-}
-chimpSwitches +=
-   ' --domainSteps=' + path.resolve('tests/step_definitions/domain') +
-   ' --criticalSteps=' + path.resolve('tests/step_definitions/critical') +
-   ' --watchSource=' + path.resolve('tests') +
-   ' --singleSnippetPerFile=1' +
-   ' --no-source';
-
-if (!process.env.CI && !process.env.TRAVIS && !process.env.CIRCLECI) {
-  // when not in Watch mode, Chimp existing will exit Meteor too
-  chimpSwitches += ' --watch';
+  startCucumber();
 }
 
-if (process.env.CIRCLECI) {
-  chimpSwitches += ' --screenshotsPath="' + process.env.CIRCLE_ARTIFACTS + '"';
+function startCucumber(callback) {
+  console.log('Running Cucumber');
+  callChimp({
+    path: path.resolve('tests/specifications'),
+    domainSteps: path.resolve('tests/step_definitions/domain'),
+    criticalSteps: path.resolve('tests/step_definitions/critical'),
+    jsonOutput: process.env.CUCUMBER_JSON_OUTPUT,
+  }, callback)
 }
 
-if (process.env.SIMIAN_API && process.env.SIMIAN_REPOSITORY) {
-  chimpSwitches += ' --simianRepositoryId=' + process.env.SIMIAN_REPOSITORY;
-  chimpSwitches += ' --simianAccessToken=' + process.env.SIMIAN_API;
+function startJasmine(callback) {
+  console.log('Running Jasmine');
+  callChimp({
+    jasmine: true,
+    path: path.resolve('tests/features'),
+  }, callback)
 }
 
-if (process.env.CUCUMBER_JSON_OUTPUT) {
-  chimpSwitches += ' --jsonOutput=' + process.env.CUCUMBER_JSON_OUTPUT;
+function startMocha(callback) {
+  console.log('Running Mocha');
+  callChimp({
+    mocha: true,
+    path: path.resolve('tests/features'),
+  }, callback)
 }
 
-// set this flag to start with a mirror locally (ala Velocity xolvio:cucumber style)
-if (process.env.WITH_MIRROR) {
-  chimpWithMirror();
-} else if (process.env.NO_METEOR) {
-  startChimp('--ddp=' + appOptions.env.ROOT_URL + chimpSwitches);
-} else {
-  // *************************************************
-  // THIS IS THE DEFAULT
-  // *************************************************
-  chimpNoMirror();
+function callChimp(chimpSwitches, callback) {
+  chimpSwitches = chimpSwitches || {};
+
+  if (features.length > 0) {
+    chimpSwitches.path = path.resolve('tests') + ' ' + features.join(" ");
+  }
+
+  chimpSwitches.watchSource = path.resolve('tests');
+  chimpSwitches.singleSnippetPerFile = 1;
+  chimpSwitches['no-source'] = true;
+
+  if (!process.env.CI && !process.env.TRAVIS && !process.env.CIRCLECI) {
+    // when not in Watch mode, Chimp existing will exit Meteor too
+    chimpSwitches.watch = true;
+  }
+
+  if (process.env.CIRCLECI) {
+    chimpSwitches.screenshotsPath= process.env.CIRCLE_ARTIFACTS;
+  }
+
+  if (process.env.SIMIAN_API && process.env.SIMIAN_REPOSITORY) {
+    chimpSwitches.simianRepositoryId = process.env.SIMIAN_REPOSITORY;
+    chimpSwitches.simianAccessToken = process.env.SIMIAN_API;
+  }
+
+  // set this flag to start with a mirror locally (ala Velocity xolvio:cucumber style)
+  if (process.env.WITH_MIRROR) {
+    chimpWithMirror(stringify(chimpSwitches));
+  } else if (process.env.NO_METEOR) {
+    startChimp('--ddp=' + appOptions.env.ROOT_URL + stringify(chimpSwitches), callback);
+  } else {
+    // *************************************************
+    // THIS IS THE DEFAULT
+    // *************************************************
+    chimpNoMirror(stringify(chimpSwitches));
+  }
 }
 
 // *************************************************
 
-function chimpWithMirror() {
+function stringify(switches) {
+  var chimpSwitches = '';
+  for (var key in switches) {
+    if (!switches[key] || !switches.hasOwnProperty(key)) continue;
+    chimpSwitches += ' --' + key + (switches[key] === true ? '' : '=' + switches[key]);
+  }
+  return chimpSwitches;
+}
+
+function chimpWithMirror(switches) {
   appOptions.waitForMessage = 'Started MongoDB';
   startApp(function () {
     startMirror(function () {
       console.log('=> Test App running at:', mirrorOptions.env.ROOT_URL);
       console.log('=> Log file: tail -f', path.resolve(mirrorOptions.logFile), '\n');
-      startChimp('--ddp=' + mirrorOptions.env.ROOT_URL + chimpSwitches);
+      startChimp('--ddp=' + mirrorOptions.env.ROOT_URL + switches);
     });
   });
 }
 
-function chimpNoMirror() {
+function chimpNoMirror(switches) {
   appOptions.waitForMessage = 'App running at';
   startApp(function () {
-    console.log("inside no mirror ", chimpSwitches);
-    startChimp('--ddp=' + appOptions.env.ROOT_URL + chimpSwitches);
+    console.log("inside no mirror ", switches);
+    startChimp('--ddp=' + appOptions.env.ROOT_URL + switches);
   });
 }
 
@@ -124,7 +161,7 @@ function startMirror(callback) {
   }, callback);
 }
 
-function startChimp(command) {
+function startChimp(command, callback) {
   console.log("chimpBin ", chimpBin);
   console.log("command ", command);
   startProcess({
@@ -137,7 +174,7 @@ function startChimp(command) {
           path.delimiter + srcDir + '/node_modules',
       }),
     },
-  });
+  }, callback);
 }
 
 function startProcess(opts, callback) {
@@ -172,4 +209,3 @@ function startProcess(opts, callback) {
   });
   processes.push(proc);
 }
-
