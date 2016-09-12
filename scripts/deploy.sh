55908fd0885b1732d42eb3ed39b11c46cb6b01b4
Sam Hatoum <sam@xolv.io>
2016-09-12 14:44:20 -0700

    Adding output info to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,9 @@
 cd src
 
 # Use Node.js to decode the Base64 environment variable called METEOR_SESSION_FILE and write it to a file called meteor-session-file.json
+echo Writing Meteor session file
 node -e "console.log(new Buffer(process.env.METEOR_SESSION_FILE_CONTENT, 'Base64').toString('utf-8'))" > meteor-session-file.json
 
+echo Deploying to $DEPLOY_SITE on Galaxy
 # Next we tell Meteor deploy to use the session file, to deploy to the $DEPLOY_SITE
-DEPLOY_HOSTNAME=galaxy.meteor.com METEOR_SESSION_FILE=meteor-session-file.json meteor deploy $DEPLOY_SITE:
-rm meteor-session-file.json
+DEPLOY_HOSTNAME=galaxy.meteor.com METEOR_SESSION_FILE=meteor-session-file.json meteor deploy $DEPLOY_SITE
