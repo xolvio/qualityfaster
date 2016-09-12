988fe9d480f22e58acbb9404e8e334b5591ea6e8
Sam Hatoum <sam@xolv.io>
2016-09-12 14:16:03 -0700

    Remove the auth file after it's used

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,3 +8,4 @@ node -e "console.log(new Buffer(process.env.METEOR_SESSION_FILE_CONTENT, 'Base64
 
 # Next we tell Meteor deploy to use the session file, to deploy to the $DEPLOY_SITE
 DEPLOY_HOSTNAME=galaxy.meteor.com METEOR_SESSION_FILE=meteor-session-file.json meteor deploy $DEPLOY_SITE:
+rm meteor-session-file.json
