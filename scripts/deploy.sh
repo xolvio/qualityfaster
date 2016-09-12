5b705e5d0b7205c9b07a3adad5664af56258b903
Sam Hatoum <sam@xolv.io>
2016-09-12 16:23:43 -0700

    Debug meteor not starting issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,9 +8,6 @@ echo Writing Meteor session file
 node -e "console.log(process.env.METEOR_SESSION_FILE_CONTENT.substring(0, 5))"
 node -e "console.log(new Buffer(process.env.METEOR_SESSION_FILE_CONTENT, 'Base64').toString('utf-8'))" > meteor-session-file.json
 
-echo Is Meteor working?
-meteor --version
-
 echo Deploying to $DEPLOY_SITE on Galaxy
 # Next we tell Meteor deploy to use the session file, to deploy to the $DEPLOY_SITE
 DEPLOY_HOSTNAME=galaxy.meteor.com METEOR_SESSION_FILE=meteor-session-file.json meteor deploy $DEPLOY_SITE
