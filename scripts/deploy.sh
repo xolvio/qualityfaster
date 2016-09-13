0626c2730dc7920e159189c45e0bf499d324700d
Sam Hatoum <sam@xolv.io>
2016-09-12 18:39:46 -0700

    Clean up after fixing Travis (yay!)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ cd src
 
 # Use Node.js to decode the Base64 environment variable called METEOR_SESSION_FILE and write it to a file called meteor-session-file.json
 echo Writing Meteor session file
-node -e "console.log(process.env.METEOR_SESSION_FILE_CONTENT.substring(0, 5))"
 node -e "console.log(new Buffer(process.env.METEOR_SESSION_FILE_CONTENT, 'Base64').toString('utf-8'))" > meteor-session-file.json
 
 echo Deploying to $DEPLOY_SITE on Galaxy
--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -4,11 +4,9 @@ if [ -f ~/.meteor/meteor ]
 then
     echo Meteor installed, restoring link
 	sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor
-	echo Restored link
 else
     echo Installing Meteor
 	curl https://install.meteor.com | sh
-	echo Installed Meteor
 fi
 
 meteor --version
