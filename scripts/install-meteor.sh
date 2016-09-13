c4641d949ef8147f9e60b40cb21c931316f8d7e9
Sam Hatoum <sam@xolv.io>
2016-09-12 17:09:51 -0700

    Debug Meteor hanging on Travis

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Restore the Meteor command if Meteor was restored from the cache
-if [ -d ~/.meteor ]; then
-  sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor
-fi
+#if [ -d ~/.meteor ]; then
+#  sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor
+#fi
 
 # Install Meteor if it's not already restored from the cache
-if [ ! -e $HOME/.meteor/meteor ]; then
-  curl https://install.meteor.com | sh
-fi
+#if [ ! -e $HOME/.meteor/meteor ]; then
+curl https://install.meteor.com | sh
+#fi
 
 which meteor
 
